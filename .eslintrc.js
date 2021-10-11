607c265ad4aaf37762e7d7fbe1becf03374c2776
Jiří Jančík <jancik@enmon.tech>
2021-10-11 22:54:56 +0200

    lintrFix

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,16 +14,11 @@ module.exports = {
     jest: true,
   },
   rules: {
-    'no-debugger': 1,
-    'no-alert': 1,
+    'no-debugger': 2,
+    'no-alert': 2,
     'no-await-in-loop': 0,
     'no-return-assign': ['error', 'except-parens'],
-    'no-restricted-syntax': [
-      2,
-      'ForInStatement',
-      'LabeledStatement',
-      'WithStatement',
-    ],
+    'no-restricted-syntax': [2, 'ForInStatement', 'LabeledStatement', 'WithStatement'],
     'no-unused-vars': [
       1,
       {
@@ -50,28 +45,28 @@ module.exports = {
         props: false,
       },
     ],
-    'no-console': 0,
+    'no-console': 1,
     'import/prefer-default-export': 0,
     import: 0,
     'func-names': 1,
     'space-before-function-paren': 0,
     'comma-dangle': 1,
-    'max-len': 120,
+    'max-len': 1,
     'import/extensions': 0,
     'no-underscore-dangle': 0,
     'consistent-return': 1,
     'react/display-name': 1,
-    'react/no-array-index-key': 1,
+    'react/no-array-index-key': 2,
     'react/react-in-jsx-scope': 0,
     'react/prefer-stateless-function': 0,
     'react/forbid-prop-types': 0,
     'react/no-unescaped-entities': 1,
     'jsx-a11y/accessible-emoji': 0,
-    "jsx-a11y/label-has-associated-control": [
-      "error",
+    'jsx-a11y/label-has-associated-control': [
+      'error',
       {
-        "assert": "either"
-      }
+        assert: 'either',
+      },
     ],
     'react/require-default-props': 0,
     'react/jsx-filename-extension': [
@@ -99,14 +94,14 @@ module.exports = {
     'prettier/prettier': [
       'error',
       {
-        trailingComma: "all",
+        trailingComma: 'all',
         singleQuote: true,
         printWidth: 120,
         semi: true,
         tabWidth: 2,
-        arrowParens: "avoid",
-        useTabs: "false",
-        //below line only for windows users facing CLRF and eslint/prettier error
+        arrowParens: 'avoid',
+        useTabs: false,
+        // below line only for windows users facing CLRF and eslint/prettier error
         // non windows users feel free to delete it
         endOfLine: 'auto',
       },
